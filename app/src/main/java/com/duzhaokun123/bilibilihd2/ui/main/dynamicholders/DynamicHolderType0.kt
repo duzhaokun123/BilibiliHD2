e17c7459ea9cbb0b058427eb508e8f6bfdfe9745
duzhaokun123 <duzhaokun2@outlook.com>
2022-04-02 02:12:18 +0800

    dynamic: todo add report unsupported type

--- app/src/main/java/com/duzhaokun123/bilibilihd2/ui/main/dynamicholders/DynamicHolderType0.kt
+++ app/src/main/java/com/duzhaokun123/bilibilihd2/ui/main/dynamicholders/DynamicHolderType0.kt
@@ -5,12 +5,19 @@ import com.duzhaokun123.bilibilihd2.R
 import com.duzhaokun123.bilibilihd2.databinding.DynamicCard0Binding
 import com.duzhaokun123.bilibilihd2.model.DynamicCardModel
 import com.duzhaokun123.bilibilihd2.ui.main.DynamicAdapter
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.microsoft.appcenter.crashes.Crashes
+import io.github.duzhaokun123.androidapptemplate.utils.TipUtil
 
 class DynamicHolderType0(context: Context) :
     DynamicAdapter.BaseDynamicHolder<DynamicCard0Binding, Any>(context, R.layout.dynamic_card_0) {
     override fun initView(
         contentBinding: DynamicCard0Binding, model: DynamicCardModel, typedCard: Any
-    ) {}
+    ) {
+        contentBinding.btnReport.setOnClickListener {
+            TipUtil.showTip(context, "TODO")
+        }
+    }
 
     override fun initData(
         contentBinding: DynamicCard0Binding, model: DynamicCardModel, typedCard: Any
